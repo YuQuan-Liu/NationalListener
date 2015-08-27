4a1c5b19b83fb6c12880c2bcc5f2dacf91ae88bf
rocket <rocket@9a3fb932-96fb-2d4d-8c8a-b92bf6ca07a8>
2015-08-27 12:01:15 +0000

    修改接收  不去查找0x68 Len Len 0x68了 如果不是以0x68 Len Len 0x68 开头的数据直接扔掉   因为tcp可靠数据传输  保证到的的顺序 如果不是以0x68 Len Len 0x68 开头的数据，认定为非法数据
    git-svn-id: https://desktop-ee61sbk:8090/svn/NationalListener/NationalListener@9 9a3fb932-96fb-2d4d-8c8a-b92bf6ca07a8

--- src/com/xdkj/national/codec/RequestDecoder.java
+++ src/com/xdkj/national/codec/RequestDecoder.java
@@ -33,74 +33,42 @@ public class RequestDecoder extends CumulativeProtocolDecoder {
 		if(!decoderState.start){
 //			System.out.println("remaining"+in.remaining());
 			if(in.remaining() >= 6){
-				for(int i = 0;i < in.remaining() && (in.remaining() - i)>5;i++){
-					if(in.get(i) == 0x68 && in.get(i+5) == 0x68){
-						int len1 = in.getShort(i+1);
-						int len2 = in.getShort(i+3);
-						if(len1 == len2){
-							decoderState.length = len1>>2;
-							decoderState.start = true;
-							decoderState.nonlength = i;
-							break;
-						}
+				if(in.get(0) == 0x68 && in.get(5) == 0x68){
+					int len1 = in.getShort(1);
+					int len2 = in.getShort(3);
+					if(len1 == len2){
+						decoderState.length = len1>>2;
+						decoderState.start = true;
 					}
 				}
-				
 				if(!decoderState.start){
 					decoderState.length = 0;
 					decoderState.start = false;
-					decoderState.nonlength = 0;
 //					in.rewind();  //remaining  会一直增加
 					in.flip();
 //					in.clear();  
 					return false;
 				}
 			}
-			if(decoderState.start){
-				if(in.remaining() >= (decoderState.length+decoderState.nonlength+8)){
-					byte[] frame = new byte[decoderState.length+8];
-//					in.get(frame,decoderState.nonlength,decoderState.length+8);
-					
-					for (int i = 0; i < frame.length; i++)
-						frame[i] = in.get(i+decoderState.nonlength);
-					in.position(decoderState.length+decoderState.nonlength+8);
-					
-					if(checkFrame(frame)){
-						//deal the frame  and  write out
-						out.write(new Frame(frame));
-						decoderState.start = false;
-						decoderState.length = 0;
-						decoderState.nonlength = 0;
-						return true;
-					}else{
-						decoderState.start = false;
-						decoderState.length = 0;
-						decoderState.nonlength = 0;
-						in.flip();
-						return false;
-					}
-				}
-			}
-		}else{
-			if(in.remaining() >= (decoderState.length+decoderState.nonlength+8)){
+		}
+		if(decoderState.start){
+			if(in.remaining() >= (decoderState.length+8)){
 				byte[] frame = new byte[decoderState.length+8];
 //				in.get(frame,decoderState.nonlength,frame.length);
 				
 				for (int i = 0; i < frame.length; i++)
-					frame[i] = in.get(i+decoderState.nonlength);
+					frame[i] = in.get(i);
 				
-				in.position(decoderState.length+decoderState.nonlength+8);
+				in.position(decoderState.length+8);
 				if(checkFrame(frame)){
 					//deal the frame  and  write out
 					out.write(new Frame(frame));
 					decoderState.start = false;
 					decoderState.length = 0;
-					decoderState.nonlength = 0;
 					return true;
 				}else{
 					decoderState.start = false;
 					decoderState.length = 0;
-					decoderState.nonlength = 0;
 					in.flip();
 					return false;
 				}
@@ -112,7 +80,6 @@ public class RequestDecoder extends CumulativeProtocolDecoder {
 	private static class DecoderState{
 		private int length;
 		private boolean start = false;
-		private int nonlength;
 	}
 	
 	private boolean checkFrame(byte[] frame){
